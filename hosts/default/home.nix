8a938da4aa9648acc363ca925364ad677cc0d5c2
Gergo Moricz <mo.geryy@gmail.com>
2024-02-26 17:12:26 +0100

    home: configure power management

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -115,6 +115,62 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+    ".config/powermanagementprofilesrc".force = true;
+    ".config/powermanagementprofilesrc".text = ''
+[AC]
+icon=battery-charging
+
+[AC][DimDisplay]
+idleTime=300000
+
+[AC][HandleButtonEvents]
+lidAction=1
+powerButtonAction=16
+powerDownAction=16
+
+[Battery]
+icon=battery-060
+
+[Battery][DPMSControl]
+idleTime=300
+lockBeforeTurnOff=0
+
+[Battery][DimDisplay]
+idleTime=120000
+
+[Battery][HandleButtonEvents]
+lidAction=1
+powerButtonAction=16
+powerDownAction=16
+
+[Battery][SuspendSession]
+idleTime=600000
+suspendThenHibernate=false
+suspendType=1
+
+[LowBattery]
+icon=battery-low
+
+[LowBattery][BrightnessControl]
+value=30
+
+[LowBattery][DPMSControl]
+idleTime=120
+lockBeforeTurnOff=0
+
+[LowBattery][DimDisplay]
+idleTime=60000
+
+[LowBattery][HandleButtonEvents]
+lidAction=1
+powerButtonAction=16
+powerDownAction=16
+
+[LowBattery][SuspendSession]
+idleTime=300000
+suspendThenHibernate=false
+suspendType=1
+'';
   };
 
   # Home Manager can also manage your environment variables through
