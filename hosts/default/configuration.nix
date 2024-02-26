4312336463274a90b5af29db21e9fe4a34f0910b
Gergo Moricz <mo.geryy@gmail.com>
2024-02-26 17:06:18 +0100

    default: add steam

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -126,6 +126,12 @@
   #  wget
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
