97671a05d77ed62465dbf917f47d10de841b429c
Gergo Moricz <mo.geryy@gmail.com>
2024-02-27 01:38:02 +0100

    fix(default/nixos): xkb rename

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -64,9 +64,9 @@
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable CUPS to print documents.
