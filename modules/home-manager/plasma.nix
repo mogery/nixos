4b7eb32bef397db6406ad5163139d670e111602f
Gergő Móricz <mo.geryy@gmail.com>
2024-03-30 21:59:46 +0100

    feat(plasma-manager): update settings

--- modules/home-manager/plasma.nix
+++ modules/home-manager/plasma.nix
@@ -8,7 +8,19 @@
     };
 
     configFile = {
-      "kcminputrc"."Mouse"."XLbInptNaturalScroll".value = true; # macOS-style scrolling
+      # Turn off pointer acceleration
+      "kcminputrc"."Mouse"."X11LibInputXAccelProfileFlat".value = true;
+      "kcminputrc"."Mouse"."XLbInptAccelProfileFlat".value = true;
+
+      # Turn on macOS-style scrolling
+      "kcminputrc"."Mouse"."XLbInptNaturalScroll".value = true;
+
+      # Night Color
+      "kwinrc"."NightColor"."Active".value = true;
+      "kwinrc"."NightColor"."Mode".value = "Location";
+      "kwinrc"."NightColor"."LatitudeFixed".value = 47.43;
+      "kwinrc"."NightColor"."LongitudeFixed".value = 18.92;
+      "kwinrc"."NightColor"."NightTemperature".value = 3000;
     };
   };
 }
