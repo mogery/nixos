bb8fc26540778ae76ef11335a2ce792e607d9495
Gergő Móricz <mo.geryy@gmail.com>
2024-09-14 20:54:53 +0200

    test

--- hosts/lune/configuration.nix
+++ hosts/lune/configuration.nix
@@ -85,59 +85,69 @@
   security.acme.defaults.email = "mo.geryy@gmail.com";
   security.acme.acceptTerms = true;
 
-  services.nginx = {
-    enable = true;
-    recommendedTlsSettings = false;
-    recommendedOptimisation = false;
-    recommendedGzipSettings = false;
-    recommendedProxySettings = false;
-    virtualHosts = {
-      # If the A and AAAA DNS records on example.org do not point on the same host as the
-      # records for myhostname.example.org, you can easily move the /.well-known
-      # virtualHost section of the code to the host that is serving example.org, while
-      # the rest stays on myhostname.example.org with no other changes required.
-      # This pattern also allows to seamlessly move the homeserver from
-      # myhostname.example.org to myotherhost.example.org by only changing the
-      # /.well-known redirection target.
-      "mogery.me" = {
-        enableACME = true;
-        forceSSL = true;
-        # This section is not needed if the server_name of matrix-synapse is equal to
-        # the domain (i.e. example.org from @foo:example.org) and the federation port
-        # is 8448.
-        # Further reference can be found in the docs about delegation under
-        # https://element-hq.github.io/synapse/latest/delegate.html
-        locations."= /.well-known/matrix/server".extraConfig = ''
-            default_type application/json;
-            add_header Access-Control-Allow-Origin *;
-            return 200 '{"m.server": "matrix.mogery.me:443"}';
-        '';
-        # This is usually needed for homeserver discovery (from e.g. other Matrix clients).
-        # Further reference can be found in the upstream docs at
-        # https://spec.matrix.org/latest/client-server-api/#getwell-knownmatrixclient
-        locations."= /.well-known/matrix/client".extraConfig = ''
-            default_type application/json;
-            add_header Access-Control-Allow-Origin *;
-            return 200 '{"m.homeserver": {"base_url": "https://matrix.mogery.me"}}';
-        '';
-      };
-      "matrix.mogery.me" = {
-        enableACME = true;
-        forceSSL = true;
-        # It's also possible to do a redirect here or something else, this vhost is not
-        # needed for Matrix. It's recommended though to *not put* element
-        # here, see also the section about Element.
-        locations."/".extraConfig = ''
-          return 404;
-        '';
-        # Forward all Matrix API calls to the synapse Matrix homeserver. A trailing slash
-        # *must not* be used here.
-        locations."/_matrix".proxyPass = "http://[::1]:8448";
-        # Forward requests for e.g. SSO and password-resets.
-        locations."/_synapse/client".proxyPass = "http://[::1]:8448";
-      };
+    services.nginx = {
+        enable = true;
+        locations."/" = {
+            return = "200 '<html><body>It works</body></html>'";
+            extraConfig = ''
+                default_type text/html;
+            '';
+        };
     };
-  };
+
+#   services.nginx = {
+#     enable = true;
+#     recommendedTlsSettings = false;
+#     recommendedOptimisation = false;
+#     recommendedGzipSettings = false;
+#     recommendedProxySettings = false;
+#     virtualHosts = {
+#       # If the A and AAAA DNS records on example.org do not point on the same host as the
+#       # records for myhostname.example.org, you can easily move the /.well-known
+#       # virtualHost section of the code to the host that is serving example.org, while
+#       # the rest stays on myhostname.example.org with no other changes required.
+#       # This pattern also allows to seamlessly move the homeserver from
+#       # myhostname.example.org to myotherhost.example.org by only changing the
+#       # /.well-known redirection target.
+#       "mogery.me" = {
+#         enableACME = true;
+#         forceSSL = true;
+#         # This section is not needed if the server_name of matrix-synapse is equal to
+#         # the domain (i.e. example.org from @foo:example.org) and the federation port
+#         # is 8448.
+#         # Further reference can be found in the docs about delegation under
+#         # https://element-hq.github.io/synapse/latest/delegate.html
+#         locations."= /.well-known/matrix/server".extraConfig = ''
+#             default_type application/json;
+#             add_header Access-Control-Allow-Origin *;
+#             return 200 '{"m.server": "matrix.mogery.me:443"}';
+#         '';
+#         # This is usually needed for homeserver discovery (from e.g. other Matrix clients).
+#         # Further reference can be found in the upstream docs at
+#         # https://spec.matrix.org/latest/client-server-api/#getwell-knownmatrixclient
+#         locations."= /.well-known/matrix/client".extraConfig = ''
+#             default_type application/json;
+#             add_header Access-Control-Allow-Origin *;
+#             return 200 '{"m.homeserver": {"base_url": "https://matrix.mogery.me"}}';
+#         '';
+#       };
+#       "matrix.mogery.me" = {
+#         enableACME = true;
+#         forceSSL = true;
+#         # It's also possible to do a redirect here or something else, this vhost is not
+#         # needed for Matrix. It's recommended though to *not put* element
+#         # here, see also the section about Element.
+#         locations."/".extraConfig = ''
+#           return 404;
+#         '';
+#         # Forward all Matrix API calls to the synapse Matrix homeserver. A trailing slash
+#         # *must not* be used here.
+#         locations."/_matrix".proxyPass = "http://[::1]:8448";
+#         # Forward requests for e.g. SSO and password-resets.
+#         locations."/_synapse/client".proxyPass = "http://[::1]:8448";
+#       };
+#     };
+#   };
 
   services.matrix-synapse = {
     extraConfigFiles = [ "/home/mogery/nixos/hosts/lune/matrix-secrets.yaml" ];
