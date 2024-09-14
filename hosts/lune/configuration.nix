7352d990f74b570a2152111361ae8119ff9a0a28
Gergő Móricz <mo.geryy@gmail.com>
2024-09-14 18:46:58 +0200

    lune: helm

--- hosts/lune/configuration.nix
+++ hosts/lune/configuration.nix
@@ -86,4 +86,15 @@
 
   services.k3s.enable = true;
   services.k3s.role = "server";
+
+  environment.systemPackages = with pkgs; [
+    (wrapHelm kubernetes-helm {
+        plugins = with pkgs.kubernetes-helmPlugins; [
+            helm-secrets
+            helm-diff
+            helm-s3
+            helm-git
+        ];
+    })
+  ];
 }
