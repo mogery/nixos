ff2759832294a274a60507a3fa0638a3df86386a
Gergő Móricz <mo.geryy@gmail.com>
2024-09-14 20:55:17 +0200

    fixx

--- hosts/lune/configuration.nix
+++ hosts/lune/configuration.nix
@@ -87,7 +87,7 @@
 
     services.nginx = {
         enable = true;
-        locations."/" = {
+        virtualHosts."/" = {
             return = "200 '<html><body>It works</body></html>'";
             extraConfig = ''
                 default_type text/html;
