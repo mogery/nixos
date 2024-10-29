6988f8224abe3e0c86e0a1b451d63734a9276304
Gergő Móricz <mo.geryy@gmail.com>
2024-10-29 18:20:18 +0100

    ts fix

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -4,11 +4,11 @@
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "both";
-    checkReversePath = "loose";
   };
-  
+
   networking.firewall = {
     trustedInterfaces = [ "tailscale0" ];
     allowedUDPPorts = [ config.services.tailscale.port ];
+    checkReversePath = "loose";
   };
 }
